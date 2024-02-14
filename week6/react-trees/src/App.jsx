5fe1c962e11a1080bbd0cf7d19a97b47f4b74b5e
Mike7704 <Michael.cowley2001@gmail.com>
2024-02-14 13:29:17 +0000

    Lists and Keys and UseEffect and Timers Demo

--- week6/react-trees/src/App.jsx
+++ week6/react-trees/src/App.jsx
@@ -1,6 +1,8 @@
 import Header from "./components/Header";
+import Timer from "./components/Timer";
 import Counter from "./components/Counter";
 import TreeCard from "./components/TreeCard";
+import { interestingTrees } from "./components/TreesArray";
 import Monkey from "./components/Monkey";
 import Footer from "./components/Footer";
 import "./App.css";
@@ -9,8 +11,10 @@ export default function App() {
   return (
     <div>
       <Header />
+      <Timer />
       <Counter />
       <Monkey />
+      {/*
       <TreeCard
         treeName="Oak"
         location="Uk"
@@ -21,6 +25,13 @@ export default function App() {
         location="Africa"
         imgLink="https://images.takeshape.io/86ce9525-f5f2-4e97-81ba-54e8ce933da7/dev/ad56c7fe-35bf-46c3-9900-67c2c2f4ae45/Green%20baobab%20at%20sunny%20day.%20Madagascar%20dreamstime_xxl_46207442%20(1).jpg?auto=compress%2Cformat&w=1200"
       />
+      
+      We can use an array instead*/}
+      <div>
+        {interestingTrees.map((tree) => (
+          <TreeCard key={tree.id} treeName={tree.name} location={tree.location} imgLink={tree.img} />
+        ))}
+      </div>
       <Footer />
     </div>
   );
---
+++ week6/react-trees/src/components/Timer.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+export default function Timer() {
+  // UseState will the the initial state of something as its argument
+  // Returns an array [value, functionToUpdate]
+  const [count, setCount] = useState(0);
+
+  // Run once (only when timer is created).
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+
+    // Return the callback function (to clean up function)
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <p>Count: {count}</p>
+    </>
+  );
+}
