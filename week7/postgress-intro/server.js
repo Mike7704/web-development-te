1404c61b5d8de7548ac46ebe84b57163ee3e4fe1
Mike7704 <Michael.cowley2001@gmail.com>
2024-02-22 15:28:49 +0000

    Relational database demo

--- week7/postgress-intro/server.js
+++ week7/postgress-intro/server.js
@@ -32,6 +32,43 @@ app.get("/seed-example", async (req, res) => {
   res.json({ message: "seeded", result });
 });
 
+// Relational database example
+
+app.get("/make-categories", async (req, res) => {
+  const result = await db.query(`CREATE TABLE IF NOT EXISTS categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);`);
+  res.json(result);
+});
+
+app.get("/make-posts", async (req, res) => {
+  const result = await db.query(`CREATE TABLE IF NOT EXISTS posts (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    category_id INTEGER REFERENCES categories(id)
+  );`);
+  res.json(result);
+});
+
+app.get("/make-tags", async (req, res) => {
+  const result = await db.query(`CREATE TABLE IF NOT EXISTS tags (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+  );`);
+  res.json(result);
+});
+
+app.get("/make-posts-tags", async (req, res) => {
+  const result = await db.query(`CREATE TABLE IF NOT EXISTS posts_tags (
+    post_id INTEGER REFERENCES posts(id),
+    tag_id INTEGER REFERENCES tags(id),
+    PRIMARY KEY (post_id, tag_id)
+  );`);
+  res.json(result);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
