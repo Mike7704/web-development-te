7a36110d703880baf326226bdeba78a335922746
Mike7704 <Michael.cowley2001@gmail.com>
2024-02-26 10:36:25 +0000

    Next App Demo

---
+++ week8/next-intro/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Next App Demo</h1>
+    </main>
+  );
+}
