561e9c74ef89e7675543734454f513db7c184d34
Mike7704 <Michael.cowley2001@gmail.com>
2024-02-27 18:08:42 +0000

    Metadata demo

--- week8/next-intro/src/app/blog/[id]/page.js
+++ week8/next-intro/src/app/blog/[id]/page.js
@@ -1,5 +1,15 @@
 import Link from "next/link";
 
+export async function generateMetadata({ params, searchParams }, parent) {
+  // load the post
+  const { rows: posts } = await sql`SELECT * FROM posts WHERE posts.Id = ${params.id}`;
+  const post = posts[0]; // get the first one
+
+  return {
+    title: post.title,
+  };
+}
+
 export default async function Page({ params }) {
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`);
   const postInfo = await response.json();
