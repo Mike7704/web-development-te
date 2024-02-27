5846f0c21c138b25b1af8a1bf2e777adc54b6aa7
Mike7704 <Michael.cowley2001@gmail.com>
2024-02-27 16:51:33 +0000

    CSS and CSS Tailwind demo

--- week8/next-intro/src/components/Header.jsx
+++ week8/next-intro/src/components/Header.jsx
@@ -1,3 +1,9 @@
+import headerStyles from "@/app/header.module.css";
+
 export default function Header() {
-  return <h1>Next App Demo</h1>;
+  return (
+    <div className={headerStyles.header}>
+      <h1 className="text-center font-bold underline">Next App Demo</h1>
+    </div>
+  );
 }
