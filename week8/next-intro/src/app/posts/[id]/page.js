0cd91fd007ce6f8aca5fd0ce8ed5bdba68420c6f
Mike7704 <Michael.cowley2001@gmail.com>
2024-03-05 11:46:26 +0000

    Error and not found page demo

--- week8/next-intro/src/app/posts/[id]/page.js
+++ week8/next-intro/src/app/posts/[id]/page.js
@@ -1,10 +1,26 @@
 import { sql } from "@vercel/postgres";
 import Link from "next/link";
+import { notFound } from "next/navigation";
 
 export default async function Post({ params }) {
   const postInfo = await sql`
   SELECT * FROM posts WHERE id = ${params.id};
 `;
+
+  // if there is no post, run the notFound function to show the not-found.jsx page.
+  if (!postInfo.rows?.[0]) {
+    notFound();
+  }
+
+  /*
+  // Trigger an error to test error page
+  function errorPageTest() {
+    // the error always throws because we're deliberately causing a problem
+    // maybe we didn't get a param and this page will not work without it
+    throw new Error("I deliberately broke this page, because I'm a bad person");
+    return <div>My page content</div>;
+  }
+  errorPageTest();*/
 
   return (
     <div>
