b4927f105498d2d75d52a4779f0effeaf716b25a
Mike7704 <Michael.cowley2001@gmail.com>
2024-03-05 11:27:27 +0000

    Radix UI component library demo.

---
+++ week9/radix-primatives/src/app/page.js
@@ -0,0 +1,11 @@
+import PopoverComponent from "@/components/Popover";
+import AccordionDemo from "@/components/Accordian";
+
+export default function Home() {
+  return (
+    <main>
+      <PopoverComponent />
+      <AccordionDemo />
+    </main>
+  );
+}
